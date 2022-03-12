59fc3878ab4327b410f09e87d72a2cdee6459077
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2022-03-13 00:53:19 +0200

    improve the title screen intro skipping module

--- src/skip-title-screen-intro.js
+++ src/skip-title-screen-intro.js
@@ -6,16 +6,24 @@ let optIdEnable = module.addOption('enable', { type: 'CHECKBOX', init: false });
 sc.TitleScreenGui.inject({
   init(...args) {
     this.parent(...args);
-    if (!sc.options.get(optIdEnable)) return;
-
-    this.removeChildGui(this.introGui);
-    this.introGui = {
-      callback: this.introGui.callback,
-      start() {
-        requestAnimationFrame(() => {
-          this.callback(ig.SEQUENCE_MSG.ENDED);
-        });
-      },
-    };
+    if (sc.options.get(optIdEnable)) {
+      this.introGui.timeLine = [{ time: 0, end: true }];
+      this.bgGui.parallax.addLoadListener({
+        onLoadableComplete: () => {
+          let { timeLine } = this.bgGui;
+          let idx = timeLine.findIndex((item) => item.time > 0);
+          if (idx < 0) idx = timeLine.length;
+          timeLine.splice(idx, 0, { time: 0, goto: 'INTRO_SKIP_NOSOUND' });
+        },
+      });
+      this.removeChildGui(this.startGui);
+      this.startGui = {
+        show: () => {
+          ig.interact.removeEntry(this.screenInteract);
+          this.buttons.show();
+        },
+        hide: () => {},
+      };
+    }
   },
 });
